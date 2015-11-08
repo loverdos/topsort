9a09c0dfbb2ea1220ae0d49a35f0a380cc4370ef
Christos KK Loverdos <loverdos@gmail.com>
2015-11-08 23:29:14 +0200

    Move LSet, LMap to their own source files

---
+++ src/main/scala/com/ckkloverdos/topsort/util/LMap.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2014 Christos KK Loverdos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ckkloverdos.topsort.util
+
+class LMap[N] private[util](
+  private[util] val map: Map[N, LSet[N]],
+  private[util] val keys: LSet[N]
+) {
+  private def get(from: N): LSet[N] = map.getOrElse(from, LSet())
+
+  def contains(n: N) = map.contains(n)
+
+  def +(fromto: (N, N)): LMap[N] = {
+    val (from, to) = fromto
+    val newSet = get(from) + to
+    val newMap = map.updated(from, newSet)
+    val newKeys = keys + from
+
+    new LMap(newMap, newKeys)
+  }
+
+  def +(a: N): LMap[N] =
+    if(map.contains(a))
+      this
+    else {
+      val newMap = map.updated(a, LSet())
+      val newKeys = keys + a
+      new LMap(newMap, newKeys)
+    }
+
+  def ++(that: LMap[N]): LMap[N] = {
+    val keys = this.keys ++ that.keys
+    val fromTos: LSet[(N, LSet[N])] = // all the keys and their concatenated LSet values
+      for(key ← keys) yield {
+        val thisSet = this.get(key)
+        val thatSet = that.get(key)
+
+        (key, thisSet ++ thatSet)
+      }
+
+    val newKeys = fromTos.map(_._1)
+    val newMap  = Map(fromTos.toSeq:_*)
+
+    new LMap(newMap, newKeys)
+  }
+
+  def ++(fromto: (N, LSet[N])): LMap[N] = {
+    val (from, to) = fromto
+    val that = new LMap(Map(from → to), LSet(from))
+
+    this ++ that
+  }
+
+  def apply(n: N): LSet[N] = map(n)
+  def dependenciesOf(node: N): LSet[N] = get(node)
+
+  def allNodes: LSet[N] = {
+    var acc = keys
+    for {
+      from ← keys
+    } {
+      val tos = map(from)
+      acc ++= tos
+    }
+    acc
+  }
+
+  def size: Int = map.size
+}
+
+object LMap {
+  def apply[N]() = new LMap[N](Map(), LSet())
+}
---
+++ src/main/scala/com/ckkloverdos/topsort/util/LSet.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 Christos KK Loverdos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ckkloverdos.topsort.util
+
+class LSet[N] private(
+  private[util] val set: Set[N],
+  private[util] val ordered: Vector[N]
+) {
+  def +(n: N): LSet[N] =
+    if(set(n))
+      this
+    else
+      new LSet(set + n, ordered :+ n)
+
+  def ++(that: Seq[N]): LSet[N] = {
+    var acc = this
+    for {
+      item ← that if !this(item)
+    } {
+      acc += item
+    }
+    acc
+  }
+
+  def ++(that: LSet[N]): LSet[N] = this ++ that.toSeq
+
+  def -(n: N): LSet[N] =
+    if(set(n))
+      new LSet(set - n, ordered.filterNot(_ == n))
+    else
+      this
+
+  def apply(n: N): Boolean = set(n)
+
+  def foreach(f: N ⇒ Unit) = ordered.foreach(f)
+  def map[S](f: N ⇒ S): LSet[S] = new LSet(set.map(f), ordered.map(f))
+  def foldLeft[Z](initial: Z)(f: (Z, N) ⇒ Z): Z = ordered.foldLeft(initial)(f)
+
+  def size = ordered.size
+
+  def toSeq: collection.immutable.Seq[N] = ordered
+  def iterator: Iterator[N] = ordered.iterator
+
+  def mkString(sep: String) = ordered.mkString(sep)
+  def mkString(start: String, sep: String, end: String) = ordered.mkString(start, sep, end)
+}
+
+object LSet {
+  final def apply[N](items: N*): LSet[N] = new LSet[N](Set(), Vector()) ++ items
+}
