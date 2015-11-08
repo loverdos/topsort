58e3c6194a533ab858c65e1b37392da1501688ee
Christos KK Loverdos <loverdos@gmail.com>
2015-11-09 00:03:35 +0200

    PrintStreamListener reports a transformed version of the node

--- src/main/scala/com/ckkloverdos/topsort/event/PrintStreamListener.scala
+++ src/main/scala/com/ckkloverdos/topsort/event/PrintStreamListener.scala
@@ -21,38 +21,45 @@ import java.io.PrintStream
 /**
  *
  */
-class PrintStreamListener[N](out: PrintStream) extends TopSortListener[N] {
-  def w(s: String, level: Int) = out.println((" " * (level * 2)) + s)
+case class PrintStreamListener[N](
+  out: PrintStream,
+  f: (N) ⇒ String = (node: N) ⇒ node.toString
+) extends TopSortListener[N] {
+  private def w(s: String, level: Int) = out.println((" " * (level * 2)) + s)
 
-  private[this] def d(dependents: List[N]) =
+  private def d(dependents: List[N]) =
     dependents match {
       case Nil ⇒ ""
-      case _ ⇒ s" (← ${dependents.mkString("←")})"
+      case _ ⇒ s" (← ${dependents.map(f).mkString("←")})"
     }
 
+  private def p(path: Traversable[N]) = path.map(f).mkString(" → ")
+
+  private def s(sorted: Traversable[N]) = sorted.map(f).mkString(", ")
+
   override def onEnter(dependents: List[N], node: N, level: Int) =
-    w(s"Enter: $node${d(dependents)}", level)
+    w(s"Enter: ${f(node)}${d(dependents)}", level)
 
   override def onCycle(path: Traversable[N], level: Int) =
-    w(s"Cycle: ${path.mkString(" → ")}", level)
+    w(s"Cycle: ${p(path)}", level)
 
   override def onAddedToSearchPath(path: Traversable[N], dependents: List[N], addedNode: N, level: Int) =
-    w(s"Search path (+ $addedNode): ${path.mkString(" → ")}", level)
+    w(s"Search path (+ ${f(addedNode)}): ${p(path)}", level)
 
   override def onRemovedFromSearchPath(path: Traversable[N], removedNode: N, level: Int) =
-    w(s"Search path (- $removedNode): ${path.mkString(" → ")}", level)
+    w(s"Search path (- ${f(removedNode)}): ${p(path)}", level)
 
   override def onAddedToSorted(dependents: List[N], node: N, level: Int) =
-    w(s"ACCEPT <$node>${d(dependents)}", level)
+    w(s"ACCEPT <${f(node)}>${d(dependents)}", level)
 
   override def onAlreadySorted(dependents: List[N], node: N, level: Int): Unit =
-    w(s"Leave already sorted: $node", level)
+    w(s"Leave already sorted: ${f(node)}", level)
 
   override def onResultSorted(sorted: Traversable[N]): Unit =
-    w(s"SORTED: ${sorted.mkString(", ")}", 0)
+    w(s"SORTED: ${s(sorted)}", 0)
 
   override def onResultCycle(path: Traversable[N]): Unit =
-    w(s"CYCLE: ${path.mkString(", ")}", 0)
+    w(s"CYCLE: ${p(path)}", 0)
 }
 
 object PrintStreamListener {
